29b5a76d3ca23aa2e09ca06e7d9893e093375d16
Richard Reynolds <xrouter@gmail.com>
2020-05-18 16:55:31 +0100

    More copy. Updated paths for scrape.js and the build script

---
+++ configuration/functions/locate.sql
@@ -0,0 +1,66 @@
+CREATE OR REPLACE FUNCTION locus_core.locate(locate_parameters JSON) RETURNS JSON AS
+$$
+DECLARE
+	results_var JSON;
+	search_ts_query tsquery;
+	default_limit INTEGER DEFAULT 10;
+    default_offset INTEGER DEFAULT 0;
+BEGIN
+
+		IF (SELECT 1 FROM   information_schema.tables WHERE  table_schema = 'locus_core' AND table_name = 'location_search_view') IS NULL THEN
+			RETURN json_build_object('error', 'Missing location search view table');
+		END IF;
+
+        IF COALESCE(locate_parameters->>'limit','') ~ '^[0-9]+$' THEN
+            default_limit = LEAST(default_limit, (locate_parameters->>'limit')::INTEGER);
+        END IF;
+
+        IF COALESCE(locate_parameters->>'offset','') ~ '^[0-9]+$' THEN
+            default_offset =  (locate_parameters->>'offset')::INTEGER;
+        END IF;
+
+        IF REPLACE(locate_parameters->>'location', ' ', '') = '' THEN
+            RETURN json_build_object('error', 'Missing location');
+        ELSE
+
+            search_ts_query = to_tsquery(
+                                     replace('(' ||
+                                            array_to_string( -- everything else joined with '|' to make optional
+                                                regexp_split_to_array( -- make AND | IN | ON as mandatory keywords
+                                                    regexp_replace( --remove non-word characters
+                                                        trim(locate_parameters->>'location') --remove spaces
+                                                        , '\W+', ' ', 'g')
+                                                    , '\W+and\W+|\W+in\W+|\W+on\W+'  ), ')&(')
+                                                || ')'
+                                            , ' ', '|')
+                                     );
+
+        END IF;
+
+	    SELECT json_build_object('type','FeatureCollection',
+                             'features', COALESCE(json_agg(
+                                            json_build_object('type',        'Feature',
+                                                              'properties',  attributes || jsonb_build_object('rank', search_rank),
+                                                              'geometry',    ST_ASGEOJSON(wkb_geometry)::JSON)
+                                            ), json_build_array())
+                            )
+        INTO results_var
+		FROM (
+
+		    SELECT jsonb_build_object('id',              id,
+		                             'location',        location,
+		                             'location_type',   location_type) AS attributes,
+		           wkb_geometry,
+		           ts_rank(tsv,search_ts_query) as search_rank
+            FROM   locus_core.location_search_view
+            WHERE  tsv @@ search_ts_query
+            ORDER BY search_rank DESC
+            OFFSET default_offset
+            LIMIT  default_limit
+		) SUB;
+
+
+		RETURN results_var;
+
+END;
+$$ LANGUAGE PLPGSQL;
---
+++ configuration/functions/reverse_geocoder.sql
@@ -0,0 +1,54 @@
+CREATE OR REPLACE FUNCTION locus_core.reverse_geocoder(search_parameters JSON) RETURNS JSON AS
+$$
+DECLARE
+	results_var JSON;
+	default_limit INTEGER DEFAULT 15;
+    default_offset INTEGER DEFAULT 0;
+    point_geometry GEOMETRY (Point, 4326);
+
+BEGIN
+
+
+
+        IF COALESCE(search_parameters->>'limit','') ~ '^[0-9]+$' THEN
+            default_limit = LEAST(default_limit, (search_parameters->>'limit')::INTEGER);
+        END IF;
+
+        IF COALESCE(search_parameters->>'offset','') ~ '^[0-9]+$' THEN
+            default_offset =  (search_parameters->>'offset')::INTEGER;
+        END IF;
+
+        IF NOT COALESCE(search_parameters->>'lon','') ~ '^[0-9\-.]+$' THEN
+            RETURN json_build_object('error', 'missing longitude');
+        END IF;
+
+        IF NOT COALESCE(search_parameters->>'lat','') ~ '^[0-9\-.]+$' THEN
+            RETURN json_build_object('error', 'missing latitude');
+        END IF;
+
+		point_geometry = ST_GEOMFROMEWKT('SRID=4326;POINT('|| (search_parameters->>'lon') ||' '||(search_parameters->>'lat')||')');
+
+	    SELECT json_build_object('type','FeatureCollection',
+                             'features', COALESCE(json_agg(
+                                            json_build_object('type',        'Feature',
+                                                              'properties',  attributes || jsonb_build_object('distance_rank', distance_rank),
+                                                              'geometry',    ST_ASGEOJSON(wkb_geometry,4326)::JSON)
+                                            ), json_build_array())
+                            )
+        INTO results_var
+		FROM (
+
+		    SELECT attributes,
+		           wkb_geometry,
+		           ST_DISTANCE(wkb_geometry::GEOGRAPHY, point_geometry::GEOGRAPHY,false) as distance_rank
+            FROM   locus_core.address_search_view
+            ORDER BY wkb_geometry <-> point_geometry
+            OFFSET default_offset
+            LIMIT  default_limit
+		) SUB;
+
+
+		RETURN results_var;
+
+END;
+$$ LANGUAGE PLPGSQL;
