6045e4a7c09b97ad9635876ab8698da4eb262ed2
Richard Reynolds <xrouter@gmail.com>
2020-05-19 16:52:12 +0100

    Simple test for api calls + more config

--- src/locus/index.js
+++ src/locus/index.js
@@ -2,7 +2,7 @@ import { Queue } from '@nautoguide/ourthings';
 import Templates from '@nautoguide/ourthings/Queueable/Templates';
 import Elements from '@nautoguide/ourthings/Queueable/Elements';
 import Internals from '@nautoguide/ourthings/Queueable/Internals';
-import Mapbox from '@nautoguide/ourthings/Queueable/Mapbox';
+import Openlayers from '@nautoguide/ourthings/Queueable/Openlayers';
 import Browser from '@nautoguide/ourthings/Queueable/Browser';
 import Api from '@nautoguide/ourthings/Queueable/Api';
 import W3Menu from '@nautoguide/ourthings/Queueable/W3Menu';
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		"api": Api,
 		"locus": Locus,
 		"internals": Internals,
-		"mapbox": Mapbox,
+		"openlayers": Openlayers,
 		"browser": Browser,
 		"w3menu":W3Menu
 
