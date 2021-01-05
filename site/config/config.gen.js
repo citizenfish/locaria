379d87c09f296ea68ff2227cff1c7aaf415c1f64
Richard Reynolds <xrouter@gmail.com>
2021-01-05 09:34:43 +0000

    Removed auto generated files

--- site/config/config.gen.js
+++
@@ -1,6 +0,0 @@
-var config={
-	"api": "https://api.surreyheath.gov.uk",
-	"baseURL": "https://api.surreyheath.gov.uksite/",
-	"center":[-0.6966581,51.3135123],
-	"ws":"wss://7y05p5gxnc.execute-api.eu-west-2.amazonaws.com/live"
-};
