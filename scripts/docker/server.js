3048dfbd66151cb20356bbe7be656fb1f45b99e5
citizenfish <dave@phased.co.uk>
2021-08-09 09:11:37 +0100

    Fix Git issues

--- scripts/docker/server.js
+++ scripts/docker/server.js
@@ -11,12 +11,29 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.use(express.json());
+global.result = {status: 'complete'};
 
 app.post('/', async (req, res) => {
 
-    let result = await load.loadOSOpenData(req.body)
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(result))
+
+    //Only allow one load at a time currently
+    if(result.status !== 'complete'){
+        res.end(JSON.stringify({message : "Another load in progress please wait until complete", status : result.status, product : result.product}))
+    }
+
+    //some of these operations can take a long time so we return immediately
+    res.end(JSON.stringify({message : "Load in progress"}));
+
+    //This is fine as long as only one call made
+    result.status = 'processing';
+    await load.loadOSOpenData(req.body);
+    result.status = 'complete';
+});
+
+app.post('/status', (req,res)=> {
+
+    res.send(JSON.stringify(result));
 });
 
 app.listen(PORT, HOST);
