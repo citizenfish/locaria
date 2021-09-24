13ad4b505cc255f88658d2139628c2993b720ecf
citizenfish <dave@nautoguide.com>
2021-09-24 11:47:54 +0100

    Reworked router docker and service start script

--- scripts/data_loading/routing/start_services.sh
+++ scripts/data_loading/routing/start_services.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "STARTING CAR on 5000"
-osrm-routed --algorithm mld  --port 5000 --max-matching-size 50000 gb-latest-car.osrm
+osrm-routed --algorithm mld  --port 5000 --max-matching-size 50000 gb-latest-car.osrm &
 echo "STARTING BICYCLE ON 5001"
-osrm-routed --algorithm mld  --port 5001 --max-matching-size 50000 gb-latest-bicycle.osrm
+osrm-routed --algorithm mld  --port 5001 --max-matching-size 50000 gb-latest-bicycle.osrm &
 echo "STARTING FOOT ON 5002"
 osrm-routed --algorithm mld  --port 5002 --max-matching-size 50000 gb-latest-foot.osrm
