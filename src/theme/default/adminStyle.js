6898b0ead54e1e94ee86a576d4713480a4b4f8f8
Richard <xrouter@gmail.com>
2022-04-08 12:09:39 +0100

    tags style for admin

--- src/theme/default/adminStyle.js
+++ src/theme/default/adminStyle.js
@@ -7,8 +7,16 @@ const theme = createTheme({
 		primary: {
 			main: '#dcbd1d'
 		},
+		secondary: {
+			main: '#328FFD',
+			lighter: '#d7f6d7'
+		},
 		text: {
 			primary: '#000'
+		},
+		selection: {
+			main: "rgb(189,123,41)",
+			lighter: "rgba(189,123,41,0.9)",
 		}
 	}
 });
