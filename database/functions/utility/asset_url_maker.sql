d39dbf2f928c7cd1c020081de8294cd7ca870242
citizenfish <dave@nautoguide.com>
2022-06-17 10:52:35 +0100

    Tabbed view for System Config

--- database/functions/utility/asset_url_maker.sql
+++ database/functions/utility/asset_url_maker.sql
@@ -1,11 +1,16 @@
 CREATE OR REPLACE FUNCTION locaria_core.asset_url_maker(array_param JSONB, asset_param JSONB, mask TEXT) RETURNS JSONB AS
 $$
 
-if(array_param === null) {
-    return []
-}
-return array_param.map((item) => {
-    return mask.replace(/_UUID_/g, item).replace('_EXT_', asset_param['ext']);
-})
+    if(array_param === null) {
+        return []
+    }
+
+    if(asset_param === null){
+        return array_param
+    }
+
+    return array_param.map((item) => {
+        return mask.replace(/_UUID_/g, item).replace('_EXT_', asset_param['ext'])
+    })
 
 $$ LANGUAGE PLV8;
