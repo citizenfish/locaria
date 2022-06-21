87bdf151dc0447b3ee50f3638ea39f93b382b909
citizenfish <dave@nautoguide.com>
2022-06-21 11:09:04 +0100

    Files trigger fix

--- database/functions/triggers/files_trigger.sql
+++ database/functions/triggers/files_trigger.sql
@@ -15,7 +15,7 @@ $$
 
         IF NEW.status = 'DOWNLOAD_REQUESTED' AND OLD.status != 'DOWNLOAD_REQUESTED' OR OLD.status IS NULL THEN
              IF (SELECT 1 FROM locaria_core.parameters WHERE parameter_name = 'fargate_config' AND (parameter->>'file_download') IS NOT NULL) IS NOT NULL THEN
-                params = params || jsonb_build_object('function', 'file_download')
+                params = params || jsonb_build_object('function', 'file_download');
                 SELECT locaria_core.aws_lambda_interface(params) INTO ret_var;
                 PERFORM locaria_core.log(ret_var || jsonb_build_object('type', 'file_download_trigger_fargate'), 'fargate_triggered');
             END IF;
@@ -28,6 +28,7 @@ $$
     END;
 $$ LANGUAGE PLPGSQL;
 
+DROP TRIGGER IF EXISTS files_trigger ON locaria_core.files;
 CREATE TRIGGER files_trigger
     AFTER INSERT OR UPDATE ON locaria_core.files
     FOR EACH ROW  WHEN ((pg_trigger_depth() = 0)) EXECUTE PROCEDURE locaria_core.files_trigger();
