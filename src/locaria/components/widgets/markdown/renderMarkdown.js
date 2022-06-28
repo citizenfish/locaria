574d1e533297e070c6bd9620cf98bc555997d08d
Richard <xrouter@gmail.com>
2022-06-28 14:25:49 +0100

    Various homepage fixes

--- src/locaria/components/widgets/markdown/renderMarkdown.js
+++ src/locaria/components/widgets/markdown/renderMarkdown.js
@@ -5,6 +5,9 @@ import RenderPlugin from "./renderPlugin";
 import Divider from "@mui/material/Divider";
 import UrlCoder from "../../../libs/urlCoder";
 import { v4 as uuidv4 } from 'uuid';
+import {useHistory} from "react-router-dom";
+import {Link} from "@mui/material";
+import TypographyLink from "../typography/typographyLink";
 
 const url = new UrlCoder();
 
@@ -16,6 +19,19 @@ export default function RenderMarkdown({markdown}) {
 	let lineId = 0;
 	for (let line in splitMarkdown) {
 		lineId++;
+
+		// extract any SX params first for the line
+
+		let sxMatch = splitMarkdown[line].match(/^\{(.*?)\}/);
+		let sx = {};
+		if (sxMatch !== null) {
+			sx = JSON.parse(sxMatch[0]);
+			splitMarkdown[line] = splitMarkdown[line].replace(sxMatch[0], '');
+		}
+
+		// let match the normal MD
+
+
 		if (splitMarkdown[line] === "") {
 			renderedMarkdown.push(
 				<p key={`md${mdid}${lineId}`}></p>
@@ -27,13 +43,6 @@ export default function RenderMarkdown({markdown}) {
 		if (match) {
 			let headerType = match[0].length - 1;
 			let cleanedMatch = splitMarkdown[line].replace(match[0], '');
-
-			let sxMatch = cleanedMatch.match(/\{(.*?)\}/);
-			let sx = {};
-			if (sxMatch !== null) {
-				sx = JSON.parse(sxMatch[0]);
-				cleanedMatch = cleanedMatch.replace(sxMatch[0], '');
-			}
 
 			renderedMarkdown.push(
 				<TypographyHeader sx={sx} element={"h" + headerType}
@@ -60,10 +69,19 @@ export default function RenderMarkdown({markdown}) {
 			continue;
 		}
 
+		match = splitMarkdown[line].match(/^\[(.*?)\]\((.*?)\)/);
+		if (match) {
+			renderedMarkdown.push(
+				<TypographyLink link={match[2]}>{match[1]}</TypographyLink>
+			);
+			continue;
+		}
+
+
 		match = splitMarkdown[line].match(/^\!\[(.*?)\]\((.*?)\)/);
 		if (match) {
 			renderedMarkdown.push(
-				<img key={`md${mdid}${lineId}`} src={url.decode(match[2],true)}/>
+				<img style={sx} key={`md${mdid}${lineId}`} src={url.decode(match[2],true)}/>
 			);
 			continue;
 		}
