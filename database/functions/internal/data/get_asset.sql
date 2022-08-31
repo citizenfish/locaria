2b58119e8421fbea69ea2d4706fb78ea9bd2288b
citizenfish <dave@nautoguide.com>
2022-08-31 11:41:14 +0100

    Get assets return multiples

--- database/functions/internal/data/get_asset.sql
+++ database/functions/internal/data/get_asset.sql
@@ -9,7 +9,9 @@ BEGIN
     SELECT jsonb_agg(jsonb_build_object('uuid', uuid) || attributes) AS attributes
     INTO ret_var
     FROM assets
-    WHERE uuid = COALESCE(parameters->>'uuid','') OR attributes @> COALESCE(parameters->'filter', jsonb_build_object('_FAIL','true'));
+    WHERE uuid = COALESCE(parameters->>'uuid','')
+    OR   parameters->'uuid' ? uuid
+    OR attributes @> COALESCE(parameters->'filter', jsonb_build_object('_FAIL','true'));
 
     RETURN jsonb_build_object('assets', COALESCE(ret_var, jsonb_build_array()));
 END;
