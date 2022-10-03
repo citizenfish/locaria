7dbb2b32b4158c19b30a5191dc364fd58a9ef143
citizenfish <dave@nautoguide.com>
2022-10-03 12:02:22 +0100

    Trap none array in asset_url_maker

--- database/functions/utility/asset_url_maker.sql
+++ database/functions/utility/asset_url_maker.sql
@@ -3,9 +3,10 @@ DROP FUNCTION IF EXISTS asset_url_maker(array_param jsonb, asset_param jsonb, ma
 CREATE OR REPLACE FUNCTION locaria_core.asset_url_maker(array_param JSONB,  mask TEXT) RETURNS JSONB AS
 $$
 
-    if(array_param === null) {
+    if(array_param === null || ! Array.isArray(array_param)) {
         return []
     }
+
 
     return array_param.map((item) => {
         //TODO this is suboptimal doing a query for every image
