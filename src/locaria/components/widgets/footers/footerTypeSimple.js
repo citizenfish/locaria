baf924e68a1908a8c0609619d8b03d8079f2ac75
Richard <xrouter@gmail.com>
2022-11-30 14:21:51 +0000

    Fixes for layout and history dupe check

--- src/locaria/components/widgets/footers/footerTypeSimple.js
+++ src/locaria/components/widgets/footers/footerTypeSimple.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import RenderMarkdown from "../markdown/renderMarkdown";
 
