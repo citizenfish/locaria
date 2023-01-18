fea143f22c5c8ad7fcc7b6e65868fb15319f7d5e
Richard <xrouter@gmail.com>
2023-01-18 09:39:06 +0000

    Start of basket work

---
+++ src/locaria/components/widgets/data/dataItemsRead/dataItemViewLink.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Button from "@mui/material/Button";
+import LaunchIcon from '@mui/icons-material/Launch';
+
+
+const DataItemViewLink = ({name,data,sx,size="small"}) => {
+    let sxActual={...{
+            color: window.systemMain.fontMain,
+            margin: "2px"
+            //fontSize: "0.5rem"
+        },...sx}
+    let dataActual=data;
+    if(typeof dataActual !=='string')
+        dataActual=JSON.stringify(dataActual);
+    return (
+        <Button size={size} variant="text" sx={sxActual} onClick={()=>{
+            window.open(dataActual,"_blank");
+        }} endIcon={<LaunchIcon />}>{name}</Button>
+    )
+}
+
+export default DataItemViewLink;
--- src/locaria/components/widgets/data/fieldView.js
+++ src/locaria/components/widgets/data/fieldView.js
@@ -32,11 +32,14 @@ import DataItemDistance from "./dataItemsRead/dataItemDistance";
 import DataItemPhoneButton from "./dataItemsRead/dataItemPhoneButton";
 import DataItemMinMedMax from "./dataItemsRead/dataItemMinMedMax";
 import DataItemCost from "./dataItemsRead/dataItemCost";
+import DataItemViewLink from "widgets/data/dataItemsRead/dataItemViewLink";
+import DataItemBasket from "widgets/data/dataItemsRead/dataItemBasket";
 
 import {v4 as uuidv4} from "uuid";
 import {useDispatch, useSelector} from "react-redux";
 import {setFormMode} from "components/redux/slices/formSlice";
-import DataItemBasket from "widgets/data/dataItemsRead/dataItemBasket";
+
+
 import ActiveBasketView from "widgets/results/activeBasketView";
 import ActiveMainResults from "widgets/results/activeMainResults";
 
@@ -220,7 +223,8 @@ const FormatField = ({field, data, mode, category, moderation}) => {
 		'minMedMax': {"element": DataItemMinMedMax},
 		'distance': {"element": DataItemDistance},
 		'cost': {"element": DataItemCost},
-		'basket': {"element": DataItemBasket}
+		'basket': {"element": DataItemBasket},
+		'viewLink': {"element": DataItemViewLink}
 	}
 
 	const dataWriteItem = {
